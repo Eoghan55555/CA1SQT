33b1d7b44dceaa83b6e8814ba47691e2e9cc1fbe
Eoghan55555 <S00233339@atu.ie>
2024-04-23 11:53:05 +0100

    Initial commit

---
+++ CA1SQT/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA1SQT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    public class DiscountServiceReal : DiscountService
+    {
+        public double GetDiscount()
+        {
+            return 0.8;
+        }
+    }
+
+    public interface DiscountService
+    {
+        double GetDiscount();
+    }
+
+    public class InsuranceService
+    {
+        public double CalcPremium(int age, string location, DiscountService ds)
+        {
+            double premium = 0.0;
+
+            if (location != "rural" && location != "urban")
+            {
+                return premium;
+            }
+            if (age < 18)
+            {
+                return premium;
+            }
+
+            if (location == "rural")
+            {
+                if ((age >= 18) && (age <= 30))
+                    premium = 5.0;
+                else if (age >= 31)
+                    premium = 2.50;
+            }
+            else if (location == "urban")
+            {
+                if ((age >= 18) && (age <= 35))
+                    premium = 6.0;
+                else if (age >= 36)
+                    premium = 5.0;
+            }
+
+            double discount = ds.GetDiscount();
+            if (age >= 50)
+                premium *= discount;
+
+            return premium;
+        }
+    }
+}
+
---
+++ CA1SQTTests/InsuranceServiceTests.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CA1SQT;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+
+namespace CA1SQT.Tests
+{
+    [TestClass()]
+    public class InsuranceServiceTests
+    {
+        
+        [TestMethod()]
+        public void CalcPremiumTest1()
+        {
+            //Arrange
+            var mockDiscountService = new Mock<DiscountService>();
+            mockDiscountService.Setup(x => x.GetDiscount()).Returns(.8);
+            InsuranceService insuranceService = new InsuranceService();
+            string location = "";
+            int age = 1;
+            double expectedpremium = 0;
+            //Act
+            double premium = insuranceService.CalcPremium(age, location, mockDiscountService.Object);
+            //Assert
+            Assert.AreEqual(premium, expectedpremium);
+        }
+        [TestMethod()]
+        public void CalcPremiumTest2()
+        {
+            //Arrange
+            var mockDiscountService = new Mock<DiscountService>();
+            mockDiscountService.Setup(x => x.GetDiscount()).Returns(.8);
+            InsuranceService insuranceService = new InsuranceService();
+            string location = "rural";
+            int age = 1;
+            double expectedpremium = 0;
+            //Act
+            double premium = insuranceService.CalcPremium(age, location, mockDiscountService.Object);
+            //Assert
+            Assert.AreEqual(premium, expectedpremium);
+        }
+        [TestMethod()]
+        public void CalcPremiumTest3()
+        {
+            //Arrange
+            var mockDiscountService = new Mock<DiscountService>();
+            mockDiscountService.Setup(x => x.GetDiscount()).Returns(.8);
+            InsuranceService insuranceService = new InsuranceService();
+            string location = "rural";
+            int age = 25;
+            double expectedpremium = 5;
+            //Act
+            double premium = insuranceService.CalcPremium(age, location, mockDiscountService.Object);
+            //Assert
+            Assert.AreEqual(premium, expectedpremium);
+        }
+        [TestMethod()]
+        public void CalcPremiumTest4()
+        {
+            //Arrange
+            var mockDiscountService = new Mock<DiscountService>();
+            mockDiscountService.Setup(x => x.GetDiscount()).Returns(.8);
+            InsuranceService insuranceService = new InsuranceService();
+            string location = "rural";
+            int age = 60;
+            double exectedpremium = 2;
+            //Act
+            double premium = insuranceService.CalcPremium(age, location, mockDiscountService.Object);
+            //Assert
+            Assert.AreEqual(premium, exectedpremium);
+        }
+        [TestMethod()]
+        public void CalcPremiumTest5()
+        {
+            //Arrange
+            var mockDiscountService = new Mock<DiscountService>();
+            mockDiscountService.Setup(x => x.GetDiscount()).Returns(.8);
+            InsuranceService insuranceService = new InsuranceService();
+            string location = "urban";
+            int age = 22;
+            double exectedpremium = 6;
+            //Act
+            double premium = insuranceService.CalcPremium(age, location, mockDiscountService.Object);
+            //Assert
+            Assert.AreEqual(premium, exectedpremium);
+        }
+        [TestMethod()]
+        public void CalcPremiumTest6()
+        {
+            //Arrange
+            var mockDiscountService = new Mock<DiscountService>();
+            mockDiscountService.Setup(x => x.GetDiscount()).Returns(.8);
+            InsuranceService insuranceService = new InsuranceService();
+            string location = "urban";
+            int age = 39;
+            double exectedpremium = 5;
+            //Act
+            double premium = insuranceService.CalcPremium(age, location, mockDiscountService.Object);
+            //Assert
+            Assert.AreEqual(premium, exectedpremium);
+        }
+    }
+}
